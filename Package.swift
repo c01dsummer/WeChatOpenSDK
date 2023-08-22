02d961c7622542887d3cb123ac820fba76e759b5
c01dsummer <xiaoyu.zone@gmail.com>
2023-08-22 17:36:40 +0800

    Update Package.swift file

--- Package.swift
+++ Package.swift
@@ -14,10 +14,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "WeChatOpenSDK"),
-        .testTarget(
-            name: "WeChatOpenSDKTests",
-            dependencies: ["WeChatOpenSDK"]),
+        .binaryTarget(
+            name: "WeChatOpenSDK",
+            path: "./Artifacts/WechatOpenSDK-XCFramework.xcframework")
     ]
 )

211bb4bffb5c3691a84359fa582395b70ca84955
c01dsummer <xiaoyu.zone@gmail.com>
2023-08-23 17:49:09 +0800

    Add test code

--- Sources/WeChatOpenSDK/WeChatOpenSDK.swift
+++ Sources/WeChatOpenSDK/WeChatOpenSDK.swift
@@ -1,2 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct WeChatOpenSDK {
+    public private(set) var title = "Hello, world!"
+    public init() {}
+}

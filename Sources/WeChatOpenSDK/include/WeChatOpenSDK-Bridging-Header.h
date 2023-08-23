83f306fc4e2133f8866315a60d23da990b546a2e
c01dsummer <xiaoyu.zone@gmail.com>
2023-08-23 16:07:00 +0800

    Update header file

---
+++ Sources/WeChatOpenSDK/include/WeChatOpenSDK-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Copyright © Jaguar Land Rover Ltd 2023 All rights reserved.
+//  This software is confidential and proprietary information.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <WechatOpenSDK-XCFramework/WXApi.h>
+#import <WechatOpenSDK-XCFramework/WXApiObject.h>
+#import <WechatOpenSDK-XCFramework/WechatAuthSDK.h>
+
+#endif /* Header_h */

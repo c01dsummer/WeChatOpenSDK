7a51fd565b608e6fdfc501d313f4f4e9acd2c614
c01dsummer <xiaoyu.zone@gmail.com>
2023-08-23 16:48:44 +0800

    Add path in package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "WeChatOpenSDK",
-            dependencies: ["WeChatOpenSDKFramework"]
+            dependencies: ["WeChatOpenSDKFramework"],
+            publicHeadersPath: "include"
         ),
         .binaryTarget(
             name: "WeChatOpenSDKFramework",

9d122c46ad0b48f4f290a1455ca188500d5c78cb
c01dsummer <xiaoyu.zone@gmail.com>
2023-08-25 09:46:06 +0800

    Update package structure

--- Package.swift
+++ Package.swift
@@ -14,13 +14,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "WeChatOpenSDK",
-            dependencies: [],
-            path: "Sources"
-        ),
         .binaryTarget(
-            name: "WeChatOpenSDKFramework",
-            path: "Artifacts/WechatOpenSDK-XCFramework.xcframework")
+            name: "WeChatOpenSDK",
+            path: "WechatOpenSDK.xcframework")
     ]
 )
---
+++ WeChatOpenSDK/WeChatOpenSDK.h
@@ -0,0 +1,18 @@
+//
+//  Copyright © Jaguar Land Rover Ltd 2023 All rights reserved.
+//  This software is confidential and proprietary information.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WechatOpenSDK.
+FOUNDATION_EXPORT double WechatOpenSDKVersionNumber;
+
+//! Project version string for WechatOpenSDK.
+FOUNDATION_EXPORT const unsigned char WechatOpenSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WechatOpenSDK/PublicHeader.h>
+
+#import "WXApi.h"
+#import "WXApiObject.h"
+#import "WechatAuthSDK.h"

23dba99958f72e0a72f22d4b7d44564a25325593
Yu Cheng <ycheng10@jaguarlandrover.com>
2023-08-28 18:04:07 +0800

    feat:update new framework

--- Package.swift
+++ Package.swift
@@ -4,18 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "WechatOpenSDK",
+    name: "WeChatOpenSDK",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "WechatOpenSDK",
-            targets: ["WechatOpenSDK"]),
+            name: "WeChatOpenSDK",
+            targets: ["WeChatOpenSDK"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-            name: "WechatOpenSDK",
-            path: "WechatOpenSDK.xcframework")
+            name: "WeChatOpenSDK",
+            path: "WeChatOpenSDK.xcframework"
+        )
     ]
 )
---
+++ create-xcframework.sh
@@ -0,0 +1,41 @@
+rm -rf build
+
+xcodebuild archive \
+-project WeChatOpenSDK.xcodeproj \
+-scheme WeChatOpenSDK \
+-destination "generic/platform=iOS" \
+-archivePath "build/iphoneos" \
+SKIP_INSTALL=NO \
+BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild archive \
+-project WeChatOpenSDK.xcodeproj \
+-scheme WeChatOpenSDK \
+-destination "generic/platform=iOS Simulator" \
+-archivePath "build/iphonesimulator" \
+SKIP_INSTALL=NO \
+BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+echo "➡️  iphoneos slice"
+xcrun lipo -i "build/iphoneos.xcarchive/Products/Library/Frameworks/WeChatOpenSDK.framework/WeChatOpenSDK"
+
+echo "⚠️  iphonesimulator slice"
+xcrun lipo -i "build/iphonesimulator.xcarchive/Products/Library/Frameworks/WeChatOpenSDK.framework/WeChatOpenSDK"
+
+echo "⚠️  remove arm64 for iphonesimulator slice"
+xcrun lipo -remove arm64 "build/iphonesimulator.xcarchive/Products/Library/Frameworks/WeChatOpenSDK.framework/WeChatOpenSDK" -o "build/iphonesimulator.xcarchive/Products/Library/Frameworks/WeChatOpenSDK.framework/WeChatOpenSDK"
+
+echo "➡️  iphonesimulator slice"
+xcrun lipo -i "build/iphonesimulator.xcarchive/Products/Library/Frameworks/WeChatOpenSDK.framework/WeChatOpenSDK"
+
+echo "✅ create xcframework"
+xcodebuild -create-xcframework \
+-framework "build/iphoneos.xcarchive/Products/Library/Frameworks/WeChatOpenSDK.framework" \
+-framework "build/iphonesimulator.xcarchive/Products/Library/Frameworks/WeChatOpenSDK.framework" \
+-output "build/WeChatOpenSDK.xcframework"
+
+cp -r build/WeChatOpenSDK.xcframework WeChatOpenSDK.xcframework
+
+zip -r WeChatOpenSDK.xcframework.zip build/WeChatOpenSDK.xcframework
+
+swift package compute-checksum WeChatOpenSDK.xcframework.zip

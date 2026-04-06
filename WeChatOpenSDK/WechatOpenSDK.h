27b3e5aee0531ce74d045f4390559bca0fcb5eeb
c01dsummer <xiaoyu.zone@gmail.com>
2026-04-06 23:56:12 +0800

    fix: use local header imports in umbrella header

--- WeChatOpenSDK/WechatOpenSDK.h
+++ WeChatOpenSDK/WechatOpenSDK.h
@@ -15,6 +15,6 @@ FOUNDATION_EXPORT const unsigned char WeChatOpenSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WeChatOpenSDK/PublicHeader.h>
 
-#import <WeChatOpenSDK/WXApi.h>
-#import <WeChatOpenSDK/WXApiObject.h>
-#import <WeChatOpenSDK/WechatAuthSDK.h>
+#import "WXApi.h"
+#import "WXApiObject.h"
+#import "WechatAuthSDK.h"
